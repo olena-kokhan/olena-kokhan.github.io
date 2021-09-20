4048713178c9e24f4d6a77ff020057e5910c10c0
Kokhana89 <44091779+Kokhana89@users.noreply.github.com>
2021-09-20 20:56:46 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,8 +4,5 @@
  * @type {import('@vue/cli-service').ProjectOptions}
  */
  module.exports = {
-   transpileDependencies: ['/node_modules/vue/dist/'],
-   publicPath: process.env.NODE_ENV === 'production'
-     ? '/portfolio/'
-     : '/'
+   transpileDependencies: ['/node_modules/vue/dist/']
  }
